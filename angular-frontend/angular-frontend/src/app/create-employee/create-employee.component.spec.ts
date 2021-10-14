a95511c689df33465075a8ea5569072ab4f04cbe
Shubham-Khaire <shubhamkhaire914@gmail.com>
2021-10-14 18:01:05 +0530

    uploading new files

---
+++ angular-frontend/angular-frontend/src/app/create-employee/create-employee.component.spec.ts
@@ -0,0 +1,27 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CreateEmployeeComponent } from './create-employee.component';
+
+describe('CreateEmployeeComponent', () => {
+  let component: CreateEmployeeComponent;
+  let fixture: ComponentFixture<CreateEmployeeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CreateEmployeeComponent ],imports:[HttpClientTestingModule,RouterTestingModule,FormsModule]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreateEmployeeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angular-frontend/angular-frontend/src/app/employee.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { EmployeeService } from './employee.service';
+
+describe('EmployeeService', () => {
+  let service: EmployeeService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({imports:[HttpClientTestingModule],providers:[EmployeeService]});
+    service = TestBed.inject(EmployeeService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
